19b9df735672f308c6e7fa16e83d447bb107c43c
Jehiah Czebotar <jehiah@gmail.com>
2015-07-02 12:29:13 -0400

    return 404s

--- src/cmd/s3_private_http/proxy.go
+++ src/cmd/s3_private_http/proxy.go
@@ -1,32 +1,64 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"io"
+	"mime"
+	"log"
+	"net/http"
+	"path"
 
-
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type Proxy struct {
 	Bucket string
-	Svc *s3.S3
+	Svc    *s3.S3
 }
 
 func (p *Proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	key := req.URL.Path
+	if key == "/" {
+		key = "/index.html"
+	}
 	resp, err := p.Svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(p.Bucket),
-		Key:    aws.String(req.URL.Path),
+		Key:    aws.String(key),
 	})
-	if err != nil {
+	if awsErr, ok := err.(awserr.Error); ok {
+		if awsErr.Code() == "NoSuchKey" {
+			http.Error(rw, "Page Not Found", 404)
+		}
+		
+		// A service error occurred.
+		log.Printf("Error: %v %v", awsErr.Code(), awsErr.Message())
+	} else if err != nil {
 		http.Error(rw, err.Error(), 500)
 		return
 	}
+
+	var contentType string
+	if resp.ContentType != nil {
+		contentType = *resp.ContentType
+	}
+
+	if contentType == "" {
+		ext := path.Ext(req.URL.Path)
+		contentType = mime.TypeByExtension(ext)
+	}
+
+	if contentType != "" {
+		rw.Header().Set("Content-Type", contentType)
+	}
+	if resp.ContentLength != nil && *resp.ContentLength > 0 {
+		rw.Header().Set("Content-Length", fmt.Sprintf("%d", resp.ContentLength))
+	}
+
 	io.Copy(rw, resp.Body)
 	resp.Body.Close()
 }
-
 
 // resp, err := svc.ListObjects(&s3.ListObjectsInput{
 // 	Bucket:  aws.String(settings.GetString("s3_bucket")),
--- src/cmd/s3_private_http/s3_private_http.go
+++ src/cmd/s3_private_http/s3_private_http.go
@@ -1,43 +1,49 @@
 package main
 
 import (
-	"net/http"
-	"time"
-	"log"
 	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	listen := flag.String("listen", ":8080", "address:port to listen on.")
 	bucket := flag.String("bucket", "", "s3 bucket name")
+	logRequests := flag.Bool("log-requests", true, "log HTTP requests")
 	flag.Parse()
-	
+
 	if *bucket == "" {
 		log.Fatalf("bucket name required")
 	}
-
 
 	svc := s3.New(&aws.Config{
 		// Credentials: creds,
 		Region: "us-east-1",
 	})
-	
-	p := &Proxy{
+
+	var h http.Handler
+	h = &Proxy{
 		Bucket: *bucket,
-		Svc: svc,
+		Svc:    svc,
 	}
-	
+	if *logRequests {
+		h = handlers.CombinedLoggingHandler(os.Stdout, h)
+	}
+
 	s := &http.Server{
 		Addr:           *listen,
-		Handler:        p,
+		Handler:        h,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-    log.Printf("listening on %s", *listen)
+	log.Printf("listening on %s", *listen)
 	log.Fatal(s.ListenAndServe())
-	
+
 }
