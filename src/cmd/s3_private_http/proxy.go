e99ac06806257bab2a1f35b51225d050ed671b4c
Jehiah Czebotar <jehiah@gmail.com>
2015-07-01 23:48:38 -0400

    start of proxy to s3 code

--- src/cmd/s3_private_http/proxy.go
+++ src/cmd/s3_private_http/proxy.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"io"
+
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type Proxy struct {
-	
+	Bucket string
+	Svc *s3.S3
 }
 
 func (p *Proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "OK")
+	resp, err := p.Svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(p.Bucket),
+		Key:    aws.String(req.URL.Path),
+	})
+	if err != nil {
+		http.Error(rw, err.Error(), 500)
+		return
+	}
+	io.Copy(rw, resp.Body)
+	resp.Body.Close()
 }
+
+
+// resp, err := svc.ListObjects(&s3.ListObjectsInput{
+// 	Bucket:  aws.String(settings.GetString("s3_bucket")),
+// 	Prefix:  aws.String("data/"),
+// 	MaxKeys: aws.Long(1000),
+// })
+// if awsErr, ok := err.(awserr.Error); ok {
+// 	// A service error occurred.
+// 	log.Fatalf("Error: %v %v", awsErr.Code, awsErr.Message)
+// } else if err != nil {
+// 	// A non-service error occurred.
+// 	log.Fatalf("%v", err)
+// }
--- src/cmd/s3_private_http/s3_private_http.go
+++ src/cmd/s3_private_http/s3_private_http.go
@@ -5,13 +5,30 @@ import (
 	"time"
 	"log"
 	"flag"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
 	listen := flag.String("listen", ":8080", "address:port to listen on.")
+	bucket := flag.String("bucket", "", "s3 bucket name")
 	flag.Parse()
 	
-	p := &Proxy{}
+	if *bucket == "" {
+		log.Fatalf("bucket name required")
+	}
+
+
+	svc := s3.New(&aws.Config{
+		// Credentials: creds,
+		Region: "us-east-1",
+	})
+	
+	p := &Proxy{
+		Bucket: *bucket,
+		Svc: svc,
+	}
 	
 	s := &http.Server{
 		Addr:           *listen,
