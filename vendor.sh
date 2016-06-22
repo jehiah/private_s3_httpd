114dde2303fc32d24564a04db0b973c8c839ec14
jc00ke <jesse@jc00ke.com>
2016-06-22 14:25:46 -0700

    Upgrade AWS SDK to 1.1.36

--- vendor.sh
+++ vendor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gb vendor fetch -tag v0.6.4 github.com/aws/aws-sdk-go/aws
-gb vendor fetch -tag v0.6.4 github.com/aws/aws-sdk-go/service/s3
+gb vendor fetch -tag v1.1.36 github.com/aws/aws-sdk-go/aws
+gb vendor fetch -tag v1.1.36 github.com/aws/aws-sdk-go/service/s3
 gb vendor fetch -revision a98ad7ee00ec53921f08832bc06ecf7fd600e6a1 github.com/vaughan0/go-ini
 gb vendor fetch --revision 8066bb491b4ea129dd71b306aab4c52868503404 github.com/gorilla/handlers
